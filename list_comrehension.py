8f48c7e5e3321f948ddb75cc9ec07b3267bbc311
Donitsu <Donitsu2002@gmail.com>
2017-06-17 17:47:52 -0400

    Create list_comprehension.py

---
+++ list_comrehension.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+
+print [ [ i, j, f] for i in range(0, x + 1) for j in range(0, y + 1) for f in range(0, z + 1) if ( ( i + j + f) != n )]
